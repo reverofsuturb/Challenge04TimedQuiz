5fad6f42f736825061841fa22e2c9623687a8c70
suturbreverof <sfriedman6711@gmail.com>
2023-02-14 19:55:50 -0700

    worked on layout, began writing vars

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+var button1 = document.querySelector("#c1")
+var button2 = document.querySelector("#c2")
+var button3 = document.querySelector("#c3")
+var button4 = document.querySelector("#c4")
+
+var questions = [
+
+  {
+    quest1: "What would you use to store multiple values in a single variable?",
+    choice1: ["Number","Oxen","Value","Array"],
+    answer1: "c"
+  },
+
+  {
+    quest2: "holder",
+    choice2: [a,b,c,d],
+    answer2: "a,b,c,d"
+  },
+
+  {
+    quest3: "holder",
+    choice3: [a,b,c,d],
+    answer3: "a"
+  },
+
+  {
+    quest4: "holder",
+    choice4: [a,b,c,d],
+    answer4: "a"
+  },
+
+  {
+    quest5: "holder",
+    choice5: [a,b,c,d],
+    answer5: "a"
+  }
+]
